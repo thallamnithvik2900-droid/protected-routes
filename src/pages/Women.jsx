113d09c3d15244057fa57b2fa8dd3458c5ba3a1c
Nithvik <thallamnithvik2900@gmail.com>
2026-03-06 13:19:58 +0530

    new

--- src/pages/Women.jsx
+++ src/pages/Women.jsx
@@ -1,7 +1,7 @@
 export default function Women() {
     return (
         <div>
-            <h1>Women's Collection</h1>
-        </div>
+            <h1>Women's  Collection</h1>
+        </div> 
     );
 }
